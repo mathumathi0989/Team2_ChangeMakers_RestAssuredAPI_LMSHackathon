12c482009619e0cec9aa802a6fe63f66ae4f37bf
Bhagyashri <bhagyashri3490@gmail.com>
2024-11-26 11:00:42 -0800

    replaced System.out statements with LoggerLoad.info in programModule

--- src/test/java/apiActions/ProgramActions.java
+++ src/test/java/apiActions/ProgramActions.java
@@ -7,7 +7,7 @@ import apiConfig.EnvConstants;
 import apiConfig.EnvVariables;
 import generics.ApiUtils;
 import generics.DataBuilder;
-
+import generics.LoggerLoad;
 import generics.jsonUtil;
 import httpRequest.BaseRequest;
 import httpRequest.RequestFactory;
@@ -34,18 +34,18 @@ public class ProgramActions {
 
 	public void setProgramID(Response response, String setProgramId) {
 		int programID = Integer.parseInt(ApiUtils.extractFromResponse(response, "programId"));
-		System.out.println("Setting programID in Env Variables");
+		LoggerLoad.info("Setting programID in Env Variables");
 		if (setProgramId.equalsIgnoreCase("UniqueProgramIdOne")) {
-			System.out.println("programIDOne is " + programID);
+			LoggerLoad.info("programIDOne is " + programID);
 			EnvVariables.programIDOne = programID;
 		} else if (setProgramId.equalsIgnoreCase("UniqueProgramIdTwo")) {
-			System.out.println("programIDTwo is " + programID);
+			LoggerLoad.info("programIDTwo is " + programID);
 			EnvVariables.programIDTwo = programID;
 		} else if (setProgramId.equalsIgnoreCase("bugProgramIDOne")) {
-			System.out.println("Bug found!! Here's programID for bug : " + programID);
+			LoggerLoad.info("Bug found!! Here's programID for bug : " + programID);
 			EnvVariables.bugProgramIDOne = programID;
 		} else {
-			System.out.println("programID is not set.");
+			LoggerLoad.info("programID is not set.");
 		}
 	}
 
@@ -57,19 +57,19 @@ public class ProgramActions {
 	public void setProgramName(Response response, String setProgramId) {
 
 		String programNAME = ApiUtils.extractFromResponse(response, "programName");
-		System.out.println("Setting programName in Env Variables");
+		LoggerLoad.info("Setting programName in Env Variables");
 
 		if (setProgramId.equalsIgnoreCase("UniqueProgramIdOne")) {
-			System.out.println("programNameOne is " + programNAME);
+			LoggerLoad.info("programNameOne is " + programNAME);
 			EnvVariables.programNameOne = programNAME;
 		} else if (setProgramId.equalsIgnoreCase("UniqueProgramIdTwo")) {
-			System.out.println("programNameTwo is " + programNAME);
+			LoggerLoad.info("programNameTwo is " + programNAME);
 			EnvVariables.programNameTwo = programNAME;
 		} else if (setProgramId.equalsIgnoreCase("bugProgramIDOne")) {
-			System.out.println("Bug found!! Here's bud's programName :  " + programNAME);
+			LoggerLoad.info("Bug found!! Here's bud's programName :  " + programNAME);
 			EnvVariables.bugProgramNameOne = programNAME;
 		} else {
-			System.out.println("programName is not set ");
+			LoggerLoad.info("programName is not set ");
 		}
 	}
 
@@ -77,14 +77,14 @@ public class ProgramActions {
 
 	public int getProgramIDOne() {
 		int prgmID = EnvVariables.programIDOne;
-		System.out.println("Reading prgmIDOne from env variables : " + prgmID);
+		LoggerLoad.info("Reading prgmIDOne from env variables : " + prgmID);
 		return prgmID;
 	}
 	/* Get ProgramIdTwo from envvariables */
 
 	public int getProgramIDTwo() {
 		int prgmID = EnvVariables.programIDTwo;
-		System.out.println("Reading prgmIDTwo from env variables : " + prgmID);
+		LoggerLoad.info("Reading prgmIDTwo from env variables : " + prgmID);
 		return prgmID;
 	}
 
@@ -92,14 +92,14 @@ public class ProgramActions {
 
 	public String getProgramNameOne() {
 		String prgmName = EnvVariables.programNameOne;
-		System.out.println("Reading prgmNameOne from env variables : " + prgmName);
+		LoggerLoad.info("Reading prgmNameOne from env variables : " + prgmName);
 		return prgmName;
 	}
 	/* Get ProgramNameTwo from envvariables */
 
 	public String getProgramNameTwo() {
 		String prgmName = EnvVariables.programNameTwo;
-		System.out.println("Reading prgmNameTwo from env variables : " + prgmName);
+		LoggerLoad.info("Reading prgmNameTwo from env variables : " + prgmName);
 		return prgmName;
 	}
 
@@ -115,18 +115,23 @@ public class ProgramActions {
 
 	public BaseRequest setDetailsToReadProgram(BaseRequest getProgram, String auth, String baseURI, String endPoint) {
 		getProgram = RequestFactory.setAuthentication(getProgram, "get", auth, baseURI);
+		LoggerLoad.info("Setting details[authentication, baseURI, endpoint ]to read all programs!!");
+		
 		return RequestFactory.createRequest(getProgram, jsonContentType, "", endPoint, "0");
 	}
 
 	public BaseRequest setDetailsToReadProgramByPrgmID(BaseRequest getProgram, String auth, String baseURI,
 			String endPoint, Integer prgramID) {
 		getProgram = RequestFactory.setAuthentication(getProgram, "get", auth, baseURI);
+		LoggerLoad.info("Setting details[authentication, baseURI, path parameter, endpoint ]to read newly created program by programID!!");
 		return RequestFactory.createRequest(getProgram, jsonContentType, "", endPoint, prgramID);
 	}
 
 	public BaseRequest setDetailsToReadProgramWithAdmins(BaseRequest getProgram, String auth, String baseURI,
 			String endPoint) {
 		getProgram = RequestFactory.setAuthentication(getProgram, "get", auth, baseURI);
+		LoggerLoad.info("Setting details[authentication, baseURI, endpoint ]to read all programs with admins!!");
+		
 		return RequestFactory.createRequest(getProgram, jsonContentType, "", endPoint, "");
 	}
 
@@ -134,15 +139,22 @@ public class ProgramActions {
 			String endPoint, String programType) {
 		postProgram = RequestFactory.setAuthentication(postProgram, "post", auth, baseURI);
 		generateAndSetValidProgramNameInEnvVariables();
-		System.out.println("New Generated ProgramName is : " + EnvVariables.programNameToCreateProgram);
+		LoggerLoad.info("New Generated ProgramName is : " + EnvVariables.programNameToCreateProgram);
+		LoggerLoad.info("Setting details[authentication, baseURI, requestBody, path parameter, endpoint ]to create new program!!");
 		String requestBody = jsonUtil.run_all_test_scenarios_from_json_data(programType);
+		LoggerLoad.info("RequestBody to create program : "+requestBody);
+		
 		return RequestFactory.createRequest(postProgram, jsonContentType, requestBody, endPoint, 0);
 	}
 
 	public BaseRequest setDetailsToUpdateProgramByID(BaseRequest putProgram, String auth, String baseURI,
 			String endPoint, String programType, Object programID) {
 		putProgram = RequestFactory.setAuthentication(putProgram, "put", auth, baseURI);
+		
+		LoggerLoad.info("Setting details[authentication, baseURI, requestBody, path parameter, endpoint ]to update program by programID!!");
 		String requestBody = jsonUtil.run_all_test_scenarios_from_json_data(programType);
+		LoggerLoad.info("Request body to send along with update request : "+requestBody);
+		
 		return RequestFactory.createRequest(putProgram, jsonContentType, requestBody, endPoint, programID);
 
 	}
@@ -150,7 +162,9 @@ public class ProgramActions {
 	public BaseRequest setDetailsToUpdateProgramByName(BaseRequest putProgram, String auth, String baseURI,
 			String endPoint, String programType, Object programName) {
 		putProgram = RequestFactory.setAuthentication(putProgram, "put", auth, baseURI);
+		LoggerLoad.info("Setting details[authentication, baseURI, requestBody, path parameter, endpoint ]to update program by programName!!");
 		String requestBody = jsonUtil.run_all_test_scenarios_from_json_data(programType);
+		LoggerLoad.info("Request body to send along with update request : "+requestBody);
 		return RequestFactory.createRequest(putProgram, jsonContentType, requestBody, endPoint, programName);
 
 	}
@@ -158,6 +172,7 @@ public class ProgramActions {
 	public BaseRequest setDetailsToDeleteProgramByID(BaseRequest deleteProgram, String auth, String baseURI,
 			String endPoint, Object programID) {
 		deleteProgram = RequestFactory.setAuthentication(deleteProgram, "delete", auth, baseURI);
+		LoggerLoad.info("Setting details[authentication, baseURI, path parameter, endpoint ]to delete program by programID!!");
 		return RequestFactory.createRequest(deleteProgram, jsonContentType, "", endPoint, programID);
 
 	}
@@ -165,6 +180,7 @@ public class ProgramActions {
 	public BaseRequest setDetailsToDeleteProgramByName(BaseRequest deleteProgram, String auth, String baseURI,
 			String endPoint, Object programName) {
 		deleteProgram = RequestFactory.setAuthentication(deleteProgram, "delete", auth, baseURI);
+		LoggerLoad.info("Setting details[authentication, baseURI, path parameter, endpoint ]to delete program by programName!!");
 		return RequestFactory.createRequest(deleteProgram, jsonContentType, "", endPoint, programName);
 
 	}
@@ -179,6 +195,10 @@ public class ProgramActions {
 		// Read the response body and update given propertyName with given propertyValue
 		JsonObject responseJson = JsonParser.parseString(response.getBody().asString()).getAsJsonObject();
 		responseJson.addProperty(propertyName, propertyValue);
+		LoggerLoad.info("Retreived body from response by programID at runtime!!");
+		LoggerLoad.info("Here's retrieved body before updation : "+responseJson.toString());
+		LoggerLoad.info("Here's retrieved body before updation : "+responseJson.toString());
+		LoggerLoad.info("Updating "+ propertyName+" to "+propertyValue);
 		return responseJson.toString();
 	}
 	
@@ -192,6 +212,7 @@ public class ProgramActions {
 		requestSpecification = getProgram.buildRequest();
 		Response response = requestSpecification.get(getProgram.getServiceUrl() + getProgram.getPathparam());
 		String requestBody = updateProgramProperty(response, propertyName, propertyValue);
+		LoggerLoad.info("Updated requestbody : "+requestBody);
 		return RequestFactory.createRequest(putProgram, jsonContentType, requestBody, endPoint, programName);
 	}
 
